3a1ae150d7178f63f67541c4cac87bc6728ab77f
Anton <antonkostiuchkov@gmail.com>
2015-10-13 15:37:03 +0300

    Optimizing js and images

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,34 @@
-$(window).scroll(function(){
+var parallaxScroll = function () {
+
   var scrolledY = $(this).scrollTop();
-  var elemPos = $('.full-time').offset().top;
-  var windowDes = $(window).height();
-  var offset = Math.min(0, scrolledY - elemPos + windowDes - 600);
+      explorePosition = $('.explore').offset().top;
+      windowHeight = $(window).height();
+      fullTimePosition = $('.full-time').offset().top;
 
-  if(scrolledY > elemPos - windowDes){
 
-    $('.globe').css({
-      'transform' : 'translate('+ offset +'px, '+ offset * -0.5 +'px)'
+  // Landing training items
+  if(scrolledY > explorePosition - (windowHeight / 1.2)) {
+
+    var trainingItem = $('.training-list .training-item');
+
+    trainingItem.each(function(i){
+
+      setTimeout(function(){
+        trainingItem.eq(i).addClass('is-showing');
+      }, (700 * (Math.exp(i * 0.14))) - 700);
     });
-    $('.notebook').css({
-      'transform' : 'translate('+ (-offset) +'px, '+ offset * -0.2 +'px)'
-    });
+
+  }
+
+  // Full-time course fly-out
+
+  if(scrolledY > fullTimePosition - windowHeight){
+
+    var offset = (Math.min(0, scrolledY - fullTimePosition + windowHeight - 500)).toFixed();
+
+    $('.globe').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 1) +'px)'});
+
+    $('.notebook').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.1) +'px)'});
     $('footer').css({
       'z-index' : '-1'
     });
@@ -21,6 +38,20 @@ $(window).scroll(function(){
       'z-index' : '-2'
     });
   }
+
+
+};
+
+
+// shim layer with setTimeout fallback. Request an animation in the next repaint.
+window.requestAnimFrame = (function(){
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
+  function(callback){
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
+
+$(window).scroll(function(){
+  window.requestAnimFrame(parallaxScroll);
 });
-
-
