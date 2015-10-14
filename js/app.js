518b2c6c6ee287aa43b1cecb9ae77d7ad9e1dba1
Anton <antonkostiuchkov@gmail.com>
2015-10-15 00:33:49 +0300

    Responsive site

--- js/app.js
+++ js/app.js
@@ -1,57 +1,58 @@
-var parallaxScroll = function () {
-
-  var scrolledY = $(this).scrollTop();
-      explorePosition = $('.explore').offset().top;
-      windowHeight = $(window).height();
-      fullTimePosition = $('.full-time').offset().top;
+$( document ).ready(function() {
 
 
-  // Landing training items
-  if(scrolledY > explorePosition - (windowHeight / 1.2)) {
-
-    var trainingItem = $('.training-list .training-item');
-
-    trainingItem.each(function(i){
-
-      setTimeout(function(){
-        trainingItem.eq(i).addClass('is-showing');
-      }, (700 * (Math.exp(i * 0.14))) - 700);
-    });
-
-  }
-
-  // Full-time course fly-out
-
-  if(scrolledY > fullTimePosition - windowHeight){
-
-    var offset = (Math.min(0, scrolledY - fullTimePosition + windowHeight - 500)).toFixed();
-
-    $('.globe').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 1) +'px)'});
-
-    $('.notebook').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.1) +'px)'});
-    $('footer').css({
-      'z-index' : '-1'
-    });
-
-  } else {
-    $('footer').css({
-      'z-index' : '-2'
-    });
-  }
+  $(window).scroll(function(){
 
 
-};
+    var scrolledY = $(this).scrollTop();
+        explorePosition = $('.explore').offset().top;
+        windowHeight = $(window).height();
+        fullTimePosition = $('.full-time').offset().top;
+        footer = $('footer');
+        footerPosition = footer.offset().top;
 
 
-// shim layer with setTimeout fallback. Request an animation in the next repaint.
-window.requestAnimFrame = (function(){
-  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
-  function(callback){
-    window.setTimeout(callback, 1000 / 60);
-  };
-})();
+    // Landing training items
+    if(scrolledY > explorePosition - (windowHeight / 1.4)) {
+
+      var trainingItem = $('.training-list .training-item');
+
+      trainingItem.each(function(i){
+
+        setTimeout(function(){
+          trainingItem.eq(i).addClass('is-showing');
+        }, (700 * (Math.exp(i * 0.14))) - 700);
+      });
+
+    }
+
+    // Full-time course fly-out
+
+    if(scrolledY > fullTimePosition - windowHeight){
+
+      var offset = (Math.min(0, scrolledY - fullTimePosition + windowHeight - 550)).toFixed();
+
+      $('.globe').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 1) +'px)'});
+
+      $('.notebook').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.1) +'px)'});
+      $('footer').css({
+        'z-index' : '-1'
+      });
+
+    } else {
+      $('footer').css({
+        'z-index' : '-2'
+      });
+    }
 
 
-$(window).scroll(function(){
-  window.requestAnimFrame(parallaxScroll);
+  });
+
+
+  $('#menu-toggle').click(function(){
+    var menu = $('.menu');
+    menu.toggleClass('menu-open');
+    return false;
+
+  });
 });
